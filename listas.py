0acef1475dc5148591853ae5ce00b1f8865ff0fc
Michelle Sánchez <42380961+A01376622@users.noreply.github.com>
2018-11-09 21:08:38 -0600

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,78 @@
+#Autor: Michelle Sánchez Guerrero
+#Descripción: Programa que emplea listas
+
+
+#Recibe una lista de números enteros y regresa una nueva lista con la suma acumulada de datos.
+def sumaracumulado(listaEnteros):
+    listaSuma = []
+    suma = 0
+    for i in listaEnteros:
+        suma += i
+        listaSuma.append(suma)
+
+    return listaSuma
+
+
+#Recibe una lista de números enteros y regresa una nueva lista sin el primer ni ultimo elemento.
+def recortarLista(listaEnteros):
+    listaRecortada = []
+
+    for k in range(1, len(listaEnteros)-1):
+        listaRecortada.append(listaEnteros[k])
+
+    return listaRecortada
+
+
+#Recibe una lista y regresa True si los valores están ordenados, False si no.
+def estanOrdenados(lista):
+    numeroAnterior = lista[0]
+
+    for i in lista:
+
+        if i < numeroAnterior:
+            return False
+        else:
+            numeroAnterior = i
+
+    return True
+
+
+#Recibe 2 cadenas y regresa True si son anagramas, False si no.
+def sonAnagramas(string1, string2):
+    str1 = string1.lower()
+    str2 = string2.lower()
+    listaStr1 = list(str1)
+    listaStr2 = list(str2)
+    listaStr2.reverse()
+
+    if listaStr1 == listaStr2:
+        return True
+    else:
+        return False
+
+
+#Recibe una lista de números enteros y regresa True si alguno de sus datos está duplicado
+def hayDuplicados(listaEnteros):
+
+    for i in listaEnteros:
+
+        numeroRepeticiones = listaEnteros.count(i)
+
+        if numeroRepeticiones > 1:
+            return True
+        else:
+            return False
+
+
+#Recibe una lista de números enteros y modifica la lista para borrar algún dato duplicado.
+def borrarDuplicados(listaEnteros):
+
+    for i in listaEnteros:
+
+        numeroRepeticiones = listaEnteros.count(i)
+
+        if numeroRepeticiones > 1:
+            listaEnteros.remove(i)
+
+    return listaEnteros
+
